473f32b1ca612e0f2072f25af3adebf37d2f1b8f
Nien <wolfzxcv@gmail.com>
2019-07-08 21:57:09 +0100

    Updates

---
+++ precache-manifest.13a125a0bd7ebd1e964fdec0ede98e48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b408ed4a27534770a8151dfb42b879d4",
+    "url": "/React-BMI-calculater/index.html"
+  },
+  {
+    "revision": "91e09b7fabf754756d87",
+    "url": "/React-BMI-calculater/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a5101281f1ac9df60c8a",
+    "url": "/React-BMI-calculater/static/js/2.0d2610f7.chunk.js"
+  },
+  {
+    "revision": "91e09b7fabf754756d87",
+    "url": "/React-BMI-calculater/static/js/main.dc186611.chunk.js"
+  },
+  {
+    "revision": "9c016eafc274bac3e179",
+    "url": "/React-BMI-calculater/static/js/runtime~main.dd2dcaf4.js"
+  }
+]);
